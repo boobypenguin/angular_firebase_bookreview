e746386d049fbba01cbc79821f3cc9538161427d
boobypenguin <boobypenguin99@gmail.com>
2019-07-01 07:19:55 +0900

    commit

---
+++ app-cli/src/app/hello/hello.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  styleUrls: ['./hello.component.css'],
+  templateUrl: './hello.component.html'
+})
+
+export class HelloComponent implements OnInit {
+  title:string;
+  message:string;
+  nowStyle:any;
+  
+  constructor() {}
+
+  ngOnInit() {
+    this.title = 'Hello-app';
+    this.message = 'false,false,false';
+    this.nowStyle = {
+      'border-style':'',
+      'border-width':'',
+      'border-color':''
+    };
+  }
+
+  check(in1, in2, in3) {
+    this.nowStyle['border-style'] = in1;
+    this.nowStyle['border-width'] = in2 + "px";
+    this.nowStyle['border-color'] = in3;
+    this.message = JSON.stringify(this.nowStyle);
+  }
+}
